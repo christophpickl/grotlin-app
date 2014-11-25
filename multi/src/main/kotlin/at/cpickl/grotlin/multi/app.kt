4fe32916eec62875bbac1fc86959db150aa3b22a
Christoph Pickl <christoph.pickl@gmail.com>
2014-11-25 16:46:33 +0100

    adding CORS support

--- multi/src/main/kotlin/at/cpickl/grotlin/multi/app.kt
+++ multi/src/main/kotlin/at/cpickl/grotlin/multi/app.kt
@@ -4,11 +4,36 @@ import com.google.inject.AbstractModule
 import at.cpickl.grotlin.multi.service.ServiceModule
 import at.cpickl.grotlin.multi.resource.ResourceModule
 import javax.ws.rs.core.Response.Status
+import org.jboss.resteasy.annotations.interception.ServerInterceptor;
+import org.jboss.resteasy.spi.interception.MessageBodyWriterInterceptor;
+import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
+import javax.ws.rs.ext.Provider;
+import org.jboss.resteasy.spi.interception.MessageBodyWriterContext
+import org.jboss.resteasy.spi.HttpRequest
+import org.jboss.resteasy.core.ResourceMethodInvoker
+import org.jboss.resteasy.core.ServerResponse
+
 
 public class AppModule : AbstractModule() {
     override fun configure() {
         install(ResourceModule())
         install(ServiceModule())
+
+        bind(javaClass<CorsInterceptor>())
+    }
+}
+
+// https://code.google.com/r/sergiobossa-terrastore-kryo/source/browse/src/main/java/terrastore/server/impl/cors/CorsInterceptor.java
+Provider ServerInterceptor public class CorsInterceptor: MessageBodyWriterInterceptor {
+    override fun write(context: MessageBodyWriterContext?) {
+        if (context == null) {
+            return
+        }
+        context.getHeaders().add("Access-Control-Allow-Origin", "*");
+        context.getHeaders().add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,UPDATE,OPTIONS");
+        context.getHeaders().add("Access-Control-Allow-Headers", "x-http-method-override");
+        context.getHeaders().add("Access-Control-Max-Age", "");
+        context.proceed()
     }
 }
 
